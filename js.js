829c956b8486f082d25c93e8fe9f826236a31126
vijaykasa <111679551+vijaykasa@users.noreply.github.com>
2023-04-13 12:26:57 +0530

    first commit

--- js.js
+++ js.js
@@ -9,3 +9,25 @@ menu.addEventListener("click", ()=>{
 window.onscroll=()=>{
     menu_bar.classList.remove("active")
 }
+
+window.onload=()=>{
+    home_left.classList.add("show")
+    setTimeout( btn.classList.add("slidup"),1000)   
+}
+
+li.forEach(Element=>{
+   
+    Element.addEventListener("click",(e)=>{
+        li.forEach(ele=>ele.classList.remove("active"))
+
+        e.target.classList.add("active")
+    })
+})
+
+window.addEventListener("scroll",()=>{
+
+        let value=window.scrollY
+        if(value>690 && value<800){
+        
+        }
+})
